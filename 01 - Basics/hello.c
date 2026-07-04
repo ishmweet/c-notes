4ba79d36b3e06d789ec8ef87ede598f1d38c6ad1
ishmweet <ishmeetsingharora07@gmail.com>
2026-07-05 00:33:35 +0530

    covered basics

---
+++ 01 - Basics/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+  // This is a  single-line comment
+
+  /*
+  this is a
+  multi-line comment
+  */
+
+  printf("Hello C! \n");
+  printf("This is my first C program. \n");
+  return 0;
+}
---
+++ 01 - Basics/variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+  int age = 24;
+  printf("You are %d years old. \n", age);
+
+  return 0;
+}
